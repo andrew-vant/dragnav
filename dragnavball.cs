3e70b077d3e662fef7e2a7820917ad3a7623a78f
Andrew Vant <ajvant@gmail.com>
2016-10-26 20:25:11 -0400

    Navball drags now...sort of
    It drags but doesn't go where I want it yet.

--- dragnavball.cs
+++ dragnavball.cs
@@ -8,23 +8,41 @@ public class NavBallAttacher : MonoBehaviour
 {
 	void Start()
 	{
-		Debug.Log("Starting draggable navball");
-		NavBall ball = GameObject.FindObjectOfType<NavBall>();
-		ball.gameObject.AddComponent<NavBallDrag>();
-		Debug.Log("Done");
+		print("Starting draggable navball");
+		GameObject ball = FindObjectOfType<NavBall>().gameObject;
+		GameObject frame = ball.transform.parent.gameObject.transform.parent.gameObject;
+
+		// Not sure which of these is actually doing the work.
+		ball.AddComponent<NavBallDrag>();
+		frame.AddComponent<NavBallDrag>();
+
+		// Show the object tree while I work out which object actually
+		// needs to be dragged...
+		string path = "/";
+		for (GameObject obj = ball.gameObject; obj != null; obj = obj.transform.parent.gameObject)
+		{
+			print(obj.GetType().Name + ":" + obj.name);
+			path = "/" + obj.GetType().Name + ":" + obj.name + path;
+		}
+		print("Done");
 	}
 }
-public class NavBallDrag : MonoBehaviour, IBeginDragHandler
+public class NavBallDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
 	public void OnBeginDrag(PointerEventData evtdata)
 	{
-		Debug.Log("Navball got begindrag");
-		//this.ptrstart = evtdata.position.x
-		//this.nbstart = this.ball.transform.position.x
+		print("Navball got begindrag");
 	}
-	public void OnMouseDrag()
+	public void OnDrag(PointerEventData evtdata)
 	{
-		Debug.Log("Navball was dragged");
-		//this.ball.transform.
+		transform.position = evtdata.position;
+	}
+	public void OnEndDrag(PointerEventData evtdata)
+	{
+		print("Drag finished");
+	}
+	public void OnDrop(PointerEventData evtdata)
+	{
+		print("Navball dropped");
 	}
 }
