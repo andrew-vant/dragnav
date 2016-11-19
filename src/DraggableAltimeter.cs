0bf62f748a811306854758b8924c1fffc0f89a5e
Andrew Vant <ajvant@gmail.com>
2016-11-19 16:48:46 -0500

    Update class names and comment documentation

--- src/DraggableAltimeter.cs
+++ src/DraggableAltimeter.cs
@@ -4,44 +4,39 @@ using Altimeter = KSP.UI.Screens.Flight.AltitudeTumbler;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-/* Two classes are needed for DraggableNavball. One is the component that
- * implements the drag behavior. I can't work out how to make it add itself
- * to the navball, so a second class (NavBallAttacher) is rigged to run at
- * flight startup solely to add the component.
- *
- * I feel like there should be a cleaner way to do this but haven't found it
- * yet.
+/* How this works: There is one class, the AltDrag component, that implements
+ * drag behavior for the altimeter. The second class, the Attacher, spawns when
+ * flight starts and adds the component to the altimeter. I can't work out how
+ * to make the dragger attach itself. I feel like there should be a cleaner way
+ * to do this but I haven't found it yet.
  */
 
 [KSPAddon(KSPAddon.Startup.Flight, false)]
-public class AltimeterAttacher : MonoBehaviour
+public class Attacher : MonoBehaviour
 {
 	void Start()
 	{
-		/* When we speak of dragging the navball, what we really want
-		 * is to drag the entire SAS/navball/maneuver control cluster.
-		 * It has the type "IVAEVACollapseGroup" but I can't figure out
-		 * where that's defined so I can't search for it. Taking the
-		 * parent of the NavBall works just as well.
-		 *
-		 * This doesn't drag the (invisible) frame around the navball's
-		 * control cluster, but that doesn't seem to hurt anything.
+		/* Rather than only dragging the altimeter, we really want to
+		 * drag the entire control cluster it's part of.  So the
+		 * dragger gets attached to that. It's the grandparent of the
+		 * altimeter object. (Found this using DebugStuff for the
+		 * curious)
 		 */
 		GameObject altimeter = GameObject.Find("Altimeter");
 		GameObject slideframeparent = altimeter.transform.parent.gameObject;
 		GameObject cluster = slideframeparent.transform.parent.gameObject;
-		cluster.AddComponent<AltDrag>();
+		cluster.AddComponent<Dragger>();
+		Destroy(gameObject);
 	}
 }
-public class AltDrag : MonoBehaviour, IBeginDragHandler, IDragHandler
+public class Dragger : MonoBehaviour, IBeginDragHandler, IDragHandler
 {
-	/* This component makes the navball draggable. Movement is restricted
-	 * to the x axis, so the ball slides along the bottom edge of the
-	 * screen.
+	/* This makes the altimeter draggable. Movement is restricted to the x
+	 * axis, so it slides along the edge of the screen.
 	 */
 
 	/* I can't figure out how to make ConfigNode.Load do the right thing
-	 * with C# properties, so NAVBALL_COORD gets mapped to a private
+	 * with C# properties, so ALTIMETER_XCOORD gets mapped to a private
 	 * variable that serves just as a place to keep save-load data.
 	 */
 	[Persistent]
@@ -62,36 +57,37 @@ public class AltDrag : MonoBehaviour, IBeginDragHandler, IDragHandler
 
 	public float xpos
 	{
-		/* Assigning to xpos moves the navball. Don't do it any other
+		/* Assigning to xpos moves the altimeter. Don't do it any other
 		 * way; stuff necessary for consistency happens here.
 		 */
 		get { return transform.position.x; }
 		set
 		{
-			// Prevent the ball from ending up offscreen where we
-			// can't drag it back.
+			// Prevent the altimeter from ending up offscreen where
+			// we can't drag it back.
 			int right_edge = GameSettings.SCREEN_RESOLUTION_WIDTH / 2;
 			int left_edge  = -right_edge;
 			if (value > right_edge) value = right_edge;
 			if (value < left_edge)  value = left_edge;
 
-			// Move the ball.
+			// Move the altimeter.
 			Vector3 newpos = transform.position;
 			newpos.x = value;
+			ALTIMETER_XCOORD = value;
 			transform.position = newpos;
 		}
 	}
 
 	public void OnBeginDrag(PointerEventData evtdata)
 	{
-		/* Record the pointer and ball positions when dragging starts.
-		 * We'll need them in OnDrag. */
+		/* Record the pointer and altimeter positions when dragging
+		 * starts.  We'll need them in OnDrag. */
 		dragstart = evtdata.position;
 		altstart = transform.position;
 	}
 	public void OnDrag(PointerEventData evtdata)
 	{
-		/* See how far the pointer has dragged and offset the navball
+		/* See how far the pointer has dragged and offset the altimeter
 		 * from its original position by that much.
 		 *
 		 * This might be cleaner with a frame-by-frame delta but I
